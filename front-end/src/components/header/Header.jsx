379e733f440b152c7f94061d36630825c552eced
1391819 <roberto.nacu@gmail.com>
2022-11-14 19:34:15 +0000

    fixed desktop/mobile search-bar responsiveness

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './header.scss';
@@ -6,9 +6,36 @@ import './header.scss';
 import HamburgerMenu from '../navigation/hamburger-menu/HamburgerMenu';
 import SearchBar from '../search-bar/SearchBar';
 import SearchBarMobile from '../search-bar/search-bar-mobile/SearchBarMobile';
-import { isMobile } from 'react-device-detect';
 
 const Header = () => {
+	const [mobileResize, setMobileResize] = useState(false);
+
+	const handleResize = () => {
+		if (window.innerWidth < 1024) {
+			setMobileResize(true);
+		} else {
+			setMobileResize(false);
+		}
+	};
+
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			window.addEventListener('resize', handleResize);
+			window.addEventListener('orientationchange', handleResize);
+			window.addEventListener('load', handleResize);
+			window.addEventListener('reload', handleResize);
+		}
+
+		return () => {
+			if (typeof window !== 'undefined') {
+				window.removeEventListener('resize', handleResize);
+				window.removeEventListener('orientationchange', handleResize);
+				window.removeEventListener('load', handleResize);
+				window.removeEventListener('reload', handleResize);
+			}
+		};
+	}, []);
+
 	return (
 		<div className="header">
 			<div className="header__wrap container">
@@ -18,7 +45,7 @@ const Header = () => {
 						<h1>KDrama Dash</h1>
 					</Link>
 				</div>
-				{isMobile ? <SearchBarMobile /> : <SearchBar />}
+				{mobileResize ? <SearchBarMobile /> : <SearchBar />}
 			</div>
 		</div>
 	);
